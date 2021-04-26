86cd8481c588cfa31713d772680a6ddecd700e03
Debbie Meda <>
2021-04-26 00:14:55 +0000

    added bootstrap styling

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const pool = dbConnection();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-//to be able to get parameters using POST method
+// To be able to get parameters using POST method
 app.use(express.urlencoded({extended: true}));
 
+// Displays main page
 app.get("/", async (req, res) => {
   res.render("index");
 });
 
+// Displays 'Add Author form from index.js 
+// then renders newAuthor page when clicked
 app.get("/author/new", async (req, res) => {
   res.render("newAuthor");
 });
 
+// Adds new author to DB
 app.post("/author/new", async (req, res) => {
   let fName = req.body.firstName;
   let lName = req.body.lastName;
@@ -36,18 +40,21 @@ app.post("/author/new", async (req, res) => {
 
 });
 
+// Displays authors in displayAuthors page
 app.get("/authors", async (req, res) => {
   let sql = `SELECT authorId, firstName, lastName FROM q_authors ORDER BY lastName ASC`;
   let rows = await executeSQL(sql);
   res.render("displayAuthors", {"authors": rows});
 });
 
+// Displays quotes in displayQuotes page
 app.get("/quotes", async (req, res) => {
   let sql = `SELECT authorId, quote, firstName, lastName FROM q_authors NATURAL JOIN q_quotes ORDER BY quote ASC`;
   let rows = await executeSQL(sql);
   res.render("displayQuotes", {"quotes": rows});
 });
 
+// Fills drop downs in newQuotes page
 app.get("/quote/addNew", async (req, res) => {
   let sql = `SELECT authorId, firstName, lastName FROM q_authors ORDER BY lastName ASC`;
   let sql2 = `SELECT DISTINCT category FROM q_quotes ORDER BY category ASC`;
@@ -56,6 +63,7 @@ app.get("/quote/addNew", async (req, res) => {
   res.render("newQuotes", {"authors": rows, "categories": rows2});
 });
 
+// Posts the quote to DB
 app.post("/quote/new", async (req, res) => {
   let q = req.body.quote;
   let c = req.body.category;
@@ -63,19 +71,23 @@ app.post("/quote/new", async (req, res) => {
   let id = req.body.authorId;
 
   let sql = `INSERT INTO q_quotes (quote, authorId, category, likes) VALUES ( ?, ?, ?, ?)`;
+  let sql2 = `SELECT authorId, firstName, lastName FROM q_authors ORDER BY lastName ASC`;
+  let sql3 = `SELECT DISTINCT category FROM q_quotes ORDER BY category ASC`;
   let params = [q, id, c, l];
   let rows = await executeSQL(sql, params);
-  res.render("newQuotes", {"message": "Quote added!"});
-
+  let rows2 = await executeSQL(sql2);
+  let rows3 = await executeSQL(sql3);
+  res.render("newQuotes", {"authors": rows2, "categories": rows3, "message": "Quote added!"});
 });
 
+// Tests if DB is connected
 app.get("/dbTest", async (req, res) => {
   let sql = "SELECT CURDATE()";
   let rows = await executeSQL(sql);
   res.send(rows);
 });//dbTest
 
-//functions
+// Functions
 async function executeSQL(sql, params){
   return new Promise (function (resolve, reject) {
     pool.query(sql, params, function (err, rows, fields) {
@@ -97,7 +109,7 @@ function dbConnection(){
 
 } //dbConnection
 
-//start server
+// Start sever, set up for deployment in Heroku 
 app.listen(process.env.PORT || 5000, () => {
 console.log("Expresss server running...")
 } )
